5689b02e260784cfb5c3c85f056412487ab5478b
Sergey Glazov <yaglazov@gmail.com>
2018-08-30 13:17:06 +0300

    шапка при кролле

--- app/scripts/app/modules/topmenu.js
+++ app/scripts/app/modules/topmenu.js
@@ -1,7 +1,25 @@
-$('._nav-hamburger').click(function (e) {
-  e.preventDefault();
-  $(this).toggleClass('is-active');
-  $('.header-nav_actions').toggleClass('is-hide');
-  $('.menu').toggleClass('is-active');
-  return false;
+$(document).ready(function() {
+  var topMenu        = $('.header--light');
+  var topMenuHeight  = topMenu.outerHeight();
+
+  var menuWrap = $('.header-nav');
+
+  $(window).scroll(function() {
+
+    if( $(window).scrollTop() > topMenuHeight ) {
+      topMenu.addClass('is-fixed');
+    } else {
+      topMenu.removeClass('is-fixed');
+    }
+
+    return false;
+  });
+
+  $('._nav-hamburger').click(function (e) {
+    e.preventDefault();
+    $(this).toggleClass('is-active');
+    $('.header-nav_actions').toggleClass('is-hide');
+    $('.menu').toggleClass('is-active');
+    return false;
+  });
 });
