899bcff322754ced0329c3dc6b443c4753c77a4d
Sergey Glazov <yaglazov@gmail.com>
2018-09-05 22:31:09 +0300

    табы

--- app/scripts/app/modules/prices.js
+++ app/scripts/app/modules/prices.js
@@ -1,5 +1,19 @@
 $(document).ready(function() {
 
-  $.ionTabs("#prices");
+  $.ionTabs("#prices", "#burial", "#cremation", {
+    type: "hash",
+  });
+
+  $('input[name="cremationtype"]').click(function () {
+    if ( $(this).prop('checked') ) {
+      $.ionTabs.setTab("types", "cremationtype");
+    }
+  });
+
+  $('input[name="burialtype"]').click(function () {
+    if ( $(this).prop('checked') ) {
+      $.ionTabs.setTab("types", "burialtype");
+    }
+  });
 
 });
