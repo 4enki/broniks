f36f71772b2d3aca60f2958079f9709504e41603
Sergey Glazov <yaglazov@gmail.com>
2018-09-06 21:46:43 +0300

    see #11

--- app/scripts/app/modules/prices.js
+++ app/scripts/app/modules/prices.js
@@ -1,31 +1,34 @@
 $(document).ready(function() {
 
-  $.ionTabs.setTab("types", "cremationtype");
+  $('#tab-container').easytabs();
 
-  $.ionTabs("#prices", {
-    type: "none",
-  });
 
-  $.ionTabs("#burial", "#cremation", {
-    type: "hash",
-  });
-
-  let cremationtype = $('input[name="cremationtype"]');
-  let burialtype = $('input[name="burialtype"]');
-
-  cremationtype.click(function () {
-    if ( $(this).prop('checked') ) {
-      $.ionTabs.setTab("types", "cremationtype");
-      burialtype.prop('checked',false);
-    }
-  });
-
-  burialtype.click(function () {
-    if ( $(this).prop('checked') ) {
-      $.ionTabs.setTab("types", "burialtype");
-      cremationtype.prop('checked',false);
-    }
-  });
+  // $.ionTabs.setTab("types", "cremationtype");
+  //
+  // $.ionTabs("#prices", {
+  //   type: "none",
+  // });
+  //
+  // $.ionTabs("#burial", "#cremation", {
+  //   type: "hash",
+  // });
+  //
+  // let cremationtype = $('input[name="cremationtype"]');
+  // let burialtype = $('input[name="burialtype"]');
+  //
+  // cremationtype.click(function () {
+  //   if ( $(this).prop('checked') ) {
+  //     $.ionTabs.setTab("types", "cremationtype");
+  //     burialtype.prop('checked',false);
+  //   }
+  // });
+  //
+  // burialtype.click(function () {
+  //   if ( $(this).prop('checked') ) {
+  //     $.ionTabs.setTab("types", "burialtype");
+  //     cremationtype.prop('checked',false);
+  //   }
+  // });
 
 });
 
