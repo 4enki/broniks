6f40aba913a21e8515454fbd3744a62231d11bc5
Sergey Glazov <yaglazov@gmail.com>
2018-09-06 22:20:25 +0300

    see #11

--- app/scripts/app/modules/prices.js
+++ app/scripts/app/modules/prices.js
@@ -1,37 +1,14 @@
 $(document).ready(function() {
 
-  $('#prices-types').easytabs({
+    $('#prices-types').easytabs({
     tabActiveClass: 'is-active',
-    tabs: '.tabs-list > .tabs-list_item'
+    tabs: '> .prices-tabs > ul > li'
   });
 
-
-  // $.ionTabs.setTab("types", "cremationtype");
-  //
-  // $.ionTabs("#prices", {
-  //   type: "none",
-  // });
-  //
-  // $.ionTabs("#burial", "#cremation", {
-  //   type: "hash",
-  // });
-  //
-  // let cremationtype = $('input[name="cremationtype"]');
-  // let burialtype = $('input[name="burialtype"]');
-  //
-  // cremationtype.click(function () {
-  //   if ( $(this).prop('checked') ) {
-  //     $.ionTabs.setTab("types", "cremationtype");
-  //     burialtype.prop('checked',false);
-  //   }
-  // });
-  //
-  // burialtype.click(function () {
-  //   if ( $(this).prop('checked') ) {
-  //     $.ionTabs.setTab("types", "burialtype");
-  //     cremationtype.prop('checked',false);
-  //   }
-  // });
+  $('#burial, #cremation').easytabs({
+    tabActiveClass: 'is-active',
+    tabs: '> .prices-tabs > ul > li'
+  });
 
 });
 
