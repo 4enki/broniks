38bf0bd8419ff085bda7c6583b248b3ec4a99ad6
Sergey Glazov <yaglazov@gmail.com>
2018-09-06 21:38:39 +0300

    смена либы табов

--- app/scripts/vendor/vendor.js
+++ app/scripts/vendor/vendor.js
@@ -1,5 +1,5 @@
 //= lib/jquery-3.3.1.min.js
 //= lib/masonry.pkgd.min.js
-//= lib/ion.tabs.min.js
+//= lib/jquery.easytabs.min.js
 //= ../../resources/assets/scripts/magnific-popup/jquery.magnific-popup.min.js
 //= ../../resources/assets/scripts/owlcarousel/owl.carousel.min.js
