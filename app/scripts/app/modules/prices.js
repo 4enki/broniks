50b8a415d2c7a38b72df3218e38df129cc08d6d5
Антон Обрядин <askadar.ru@gmail.com>
2018-09-13 21:22:05 +0300

    Jitter fix to #15
    Not sure what was causing it, probably easytabs being picky with configuration and thus failing to prevent anchor scroll

--- app/scripts/app/modules/prices.js
+++ app/scripts/app/modules/prices.js
@@ -2,30 +2,33 @@ $(document).ready(function() {
   var savedHash = location.hash || '#';
   var hashParts = savedHash.split('_');
   var savedPanel;
-  $('#prices-types').easytabs({
-	defaultTab: savedHash.length > 1 ? hashParts[0] + '-selector' : null,
+
+  var config = {
     updateHash: false,
     tabActiveClass: 'is-active',
     tabs: '.headline_tabs .switch-list > li'
-}).bind('easytabs:before', function(a,b,c) {
-	savedPanel = location.hash;
-	// console.log(this, a,b,c);
-}).bind('easytabs:after', function(a,b,c) {
-	var nextId = '#' + c.attr('id');
-	var next = nextId + '_' + savedPanel.split('_')[1];
-	$(nextId + '1').easytabs('select', next);
-	// history.pushState({}, next);
-});
-
-  $('#burial1, #cremation1').easytabs({
-    tabActiveClass: 'is-active',
-    tabs: '> .prices-tabs > ul > li'
+  };
+  if (savedHash.length > 1)
+    config.defaultTab = hashParts[0] + '-selector';
+  $('#prices-types').easytabs(config).bind('easytabs:before', function(a, b, c) {
+    if (c.attr('id').indexOf('_') > -1)
+      return;
+    savedPanel = location.hash;
+  }).bind('easytabs:after', function(a, b, c) {
+    if (c.attr('id').indexOf('_') > -1)
+      return;
+    var nextId = '#' + c.attr('id');
+    var next = nextId + '_' + savedPanel.split('_')[1];
+    $(nextId + '1').easytabs('select', next);
+    // console.log(savedPanel, '->' , next, a,b,c, this);
+    // history.pushState({}, next);
   });
 
-  $('#costs-list').easytabs({
-    updateHash: false,
-    tabActiveClass: 'is-active',
-    tabs: '.headline_tabs .switch-list > li'
-  });
+  $('#burial1, #cremation1').easytabs({tabActiveClass: 'is-active', tabs: '> .prices-tabs > ul > li'});
 
+  $('#costs-list').easytabs({updateHash: false, tabActiveClass: 'is-active', tabs: '.headline_tabs .switch-list > li'});
+
+  $('html, body').stop().animate({
+    scrollTop: $('#prices-types').offset().top - 143
+  }, 300, 'swing');
 });
