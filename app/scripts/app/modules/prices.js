dfedb15e1fc33a3b97c385ddebbebe0bef50fca1
Антон Обрядин <askadar.ru@gmail.com>
2018-09-13 11:07:58 +0300

    #15 synced tabs to hash on page load
    Edited several ids:
    cremationtype -> cremation
    cremation -> cremation1
    Added ids to all tabs selectors
    #cremation-selector
    #creamation_optimally-selector
    and similar

--- app/scripts/app/modules/prices.js
+++ app/scripts/app/modules/prices.js
@@ -1,12 +1,14 @@
 $(document).ready(function() {
-
+  var savedHash = location.hash || '#';
+  var hashParts = savedHash.split('_');
   $('#prices-types').easytabs({
+	defaultTab: savedHash.length > 1 ? hashParts[0] + '-selector' : null,
     updateHash: false,
     tabActiveClass: 'is-active',
     tabs: '.headline_tabs .switch-list > li'
   });
 
-  $('#burial, #cremation').easytabs({
+  $('#burial1, #cremation1').easytabs({
     tabActiveClass: 'is-active',
     tabs: '> .prices-tabs > ul > li'
   });
