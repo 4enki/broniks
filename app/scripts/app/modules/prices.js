c5d263b76ef68e554788fda2a66d2ec7f7edaf2a
Антон Обрядин <askadar.ru@gmail.com>
2018-09-13 11:09:17 +0300

    #14
    Added events to easytabs
    easytabs:before when switch started, save current hash - current tab
    easytabs:after when tab switched, load previous hash and select sub-tab in new current tab

--- app/scripts/app/modules/prices.js
+++ app/scripts/app/modules/prices.js
@@ -1,12 +1,21 @@
 $(document).ready(function() {
   var savedHash = location.hash || '#';
   var hashParts = savedHash.split('_');
+  var savedPanel;
   $('#prices-types').easytabs({
 	defaultTab: savedHash.length > 1 ? hashParts[0] + '-selector' : null,
     updateHash: false,
     tabActiveClass: 'is-active',
     tabs: '.headline_tabs .switch-list > li'
-  });
+}).bind('easytabs:before', function(a,b,c) {
+	savedPanel = location.hash;
+	// console.log(this, a,b,c);
+}).bind('easytabs:after', function(a,b,c) {
+	var nextId = '#' + c.attr('id');
+	var next = nextId + '_' + savedPanel.split('_')[1];
+	$(nextId + '1').easytabs('select', next);
+	// history.pushState({}, next);
+});
 
   $('#burial1, #cremation1').easytabs({
     tabActiveClass: 'is-active',
